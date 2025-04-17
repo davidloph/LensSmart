dbef56b0c282eb932337ceb16ea08f0c5755733b
David Lopera <davidloperahenao@gmail.com>
2025-04-17 13:48:25 -0500

    Cargar línea base
    Cargar arquetipo inicial de la línea base para comenzar a evolucionar.

---
+++ lensSmart/src/main/java/com/example/initializer/LensSmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LensSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LensSmartApplication.class, args);
+	}
+
+}
